31e545c8f532cc51b61936e63397b260538a24df
voskresenie07 <123150989+voskresenie07@users.noreply.github.com>
2023-03-25 21:38:12 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH := $(call my-dir)
-ifneq ($(filter j7toplteskt,$(TARGET_DEVICE)),)
+ifneq ($(filter j7topltekor,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
